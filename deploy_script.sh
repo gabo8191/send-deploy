dd3e0af66b16de2e31cab670e8aadce1aff9641c
Gabriel Castillo Mendieta <gcastillo@serempre.com>
2024-09-11 21:26:33 -0500

    first commit

--- deploy_script.sh
+++ deploy_script.sh
@@ -6,15 +6,15 @@ echo "Script started"
 
 echo "$PASSWORD" | sudo -S apt-get update -y
 
-if ! command -v nginx &> /dev/null
+if ! command -v apache2 &> /dev/null
 then
-    echo "$PASSWORD" | sudo -S apt-get install -y nginx
-    echo "Nginx installed"
+    echo "$PASSWORD" | sudo -S apt-get install -y apache2
+    echo "Apache2 installed"
 fi
 
 REPO_DIR="/var/www/html/app"
 REPO_URL="https://github.com/gabo8191/test-ssh.git"
-NGINX_CONF="/etc/nginx/sites-available/default"
+APACHE_CONF="/etc/apache2/sites-available/000-default.conf"
 
 if [ ! -d "$REPO_DIR" ]; then
   echo "$PASSWORD" | sudo -S git clone $REPO_URL $REPO_DIR
@@ -28,23 +28,21 @@ fi
 echo "$PASSWORD" | sudo -S chown -R www-data:www-data $REPO_DIR
 echo "$PASSWORD" | sudo -S chmod -R 755 $REPO_DIR
 
-echo "$PASSWORD" | sudo -S tee $NGINX_CONF > /dev/null <<EOT
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root $REPO_DIR;
-    index index.html;
-
-    server_name localhost;
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-}
+echo "$PASSWORD" | sudo -S tee $APACHE_CONF > /dev/null <<EOT
+<VirtualHost *:80>
+    ServerAdmin webmaster@localhost
+    DocumentRoot $REPO_DIR
+    <Directory $REPO_DIR>
+        Options Indexes FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+    ErrorLog \${APACHE_LOG_DIR}/error.log
+    CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
 EOT
 
-echo "$PASSWORD" | sudo -S nginx -t
-echo "$PASSWORD" | sudo -S systemctl restart nginx
+echo "$PASSWORD" | sudo -S apache2ctl configtest
+echo "$PASSWORD" | sudo -S systemctl restart apache2
 
-echo "The app has been deployed and Nginx is configured to serve on localhost"
+echo "The app has been deployed and Apache2 is configured to serve on localhost"
